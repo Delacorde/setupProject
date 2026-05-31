7e4ba842f90115458e3f66849d40e4c8a6e9f6cb
delacorde <tripovd@gmail.com>
2026-05-31 22:18:25 +0700

    Initial Commit

---
+++ setupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupProject
+//
+//  Created by it on 31.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
