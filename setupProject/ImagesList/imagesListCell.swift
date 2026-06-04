46d5904da0c2561a57f9e20a76a491d3de188b30
delacorde <tripovd@gmail.com>
2026-06-05 02:13:54 +0700

    sprint_08 completed

---
+++ setupProject/ImagesList/imagesListCell.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var dateLabelOutlet: UILabel!
+    @IBOutlet weak var LikeButtonOutlet: UIButton!
+    @IBOutlet weak var imageCellOutlet: UIImageView!
+}
