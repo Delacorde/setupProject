22c08a91f826de67bb87fcd644cb0b55e0b872a7
delacorde <tripovd@gmail.com>
2026-06-06 14:09:40 +0700

    fixed constraints, min deployments

--- setupProject/ImagesList/imagesListCell.swift
+++ setupProject/ImagesList/imagesListCell.swift
@@ -3,7 +3,7 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
     
+    @IBOutlet weak var imageCellOutlet: UIImageView!
     @IBOutlet weak var dateLabelOutlet: UILabel!
     @IBOutlet weak var LikeButtonOutlet: UIButton!
-    @IBOutlet weak var imageCellOutlet: UIImageView!
 }
