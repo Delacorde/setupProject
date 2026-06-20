9c60903c3c435bf8312ea65abf5d0fbd24bc9823
delacorde <tripovd@gmail.com>
2026-06-20 15:09:19 +0700

    sprint 09

--- setupProject/ImagesList/ImagesListViewController.swift
+++ setupProject/ImagesList/ImagesListViewController.swift
@@ -8,6 +8,7 @@ class ImagesListViewController: UIViewController {
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private let photosName: [String] = Array(0..<20).map{"\($0)"}
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -15,6 +16,22 @@ class ImagesListViewController: UIViewController {
         formatter.timeStyle = .none
         return formatter
     }()
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier{
+               guard
+                   let viewController = segue.destination as? SingleImageViewController,
+                   let indexPath = sender as? IndexPath
+               else {
+                   assertionFailure("Invalid segue destination")
+                   return
+               }
+               let image = UIImage(named: photosName[indexPath.row])
+               viewController.image = image
+           } else {
+               super.prepare(for: segue, sender: sender)
+           }
+       }
+   
 }
 extension ImagesListViewController{
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath){
@@ -32,8 +49,8 @@ extension ImagesListViewController{
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
+            performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+        }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return 0
@@ -61,4 +78,3 @@ extension ImagesListViewController: UITableViewDataSource{
         
     }
 }
-
---
+++ setupProject/SingleImageViewController.swift
@@ -0,0 +1,72 @@
+import UIKit
+final class SingleImageViewController: UIViewController {
+    // MARK: Outlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var backwardOutlet: UIButton!
+    @IBOutlet weak var didTapButtonOutlet: UIButton!
+    var image: UIImage? {
+            didSet {
+                guard isViewLoaded, let image else { return }
+                
+                imageView.image = image
+                imageView.frame.size = image.size
+                rescaleAndCenterImageInScrollView(image: image)
+            }
+        }
+    @IBOutlet weak var imageView: UIImageView!
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+
+        guard let image else {return}
+        imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
+
+    }
+    
+    //MARK: Properties
+    
+    
+    
+    
+    
+    //MARK: funcs
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    // MARK: Actions
+    @IBAction func backwardAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image = image else{return}
+        let activityToShare: [Any] = [image]
+        let activityController = UIActivityViewController(activityItems: activityToShare, applicationActivities: nil)
+        
+        present(activityController,animated: true)
+    }
+    
+}
+//MARK: Extenctions
+extension SingleImageViewController: UIScrollViewDelegate{
+    func viewForZooming(in scrollView: UIScrollView)->UIView? {
+        imageView
+    }
+}
