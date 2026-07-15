3bb523447c3341475a6a22674d6f37ecbde0ed23
delacorde <tripovd@gmail.com>
2026-07-15 17:55:24 +0700

    sprint_10 review_1

--- setupProject/Profile/ProfileViewController.swift
+++ setupProject/Profile/ProfileViewController.swift
@@ -3,11 +3,11 @@ final class ProfileViewController: UIViewController{
     override func viewDidLoad(){
         //MARK: IMAGE
         let profileImage = UIImage(named: "profilePhoto")
-        let avatarImageView = UIImageView(image: profileImage)
-        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(avatarImageView)
-        avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 32).isActive = true
+        let image = UIImageView(image: profileImage)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
+        image.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 32).isActive = true
         
         //MARK:LABELS
         let nameLabel = UILabel()
@@ -19,44 +19,44 @@ final class ProfileViewController: UIViewController{
         nameLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 16).isActive = true
         nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 124).isActive = true
-        nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor,constant: 8).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: image.bottomAnchor,constant: 8).isActive = true
         
-        let LoginNameLabel = UILabel()
-        LoginNameLabel.font = UIFont(name: "SF-Pro-Display-Regular", size: 13)
-        LoginNameLabel.textColor = .ypGray
-        LoginNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(LoginNameLabel)
-        LoginNameLabel.text = "@ekaterina_nov"
-        LoginNameLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
-        LoginNameLabel.leadingAnchor.constraint(equalTo: LoginNameLabel.leadingAnchor).isActive = true
-        LoginNameLabel.topAnchor.constraint(equalTo: LoginNameLabel.bottomAnchor, constant: 8).isActive = true
+        let nickName = UILabel()
+        nickName.font = UIFont(name: "SF-Pro-Display-Regular", size: 13)
+        nickName.textColor = .ypGray
+        nickName.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nickName)
+        nickName.text = "@ekaterina_nov"
+        nickName.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        nickName.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        nickName.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
         
-        let descriptionLabel = UILabel()
-        descriptionLabel.font = UIFont(name: "SF-Pro-Display-Regular", size: 13)
-        descriptionLabel.textColor = .white
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(descriptionLabel)
-        descriptionLabel.text = "Hello World!"
-        descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: LoginNameLabel.bottomAnchor,constant: 8).isActive = true
+        let bio = UILabel()
+        bio.font = UIFont(name: "SF-Pro-Display-Regular", size: 13)
+        bio.textColor = .white
+        bio.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bio)
+        bio.text = "Hello World!"
+        bio.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        bio.topAnchor.constraint(equalTo: nickName.bottomAnchor,constant: 8).isActive = true
         
         let buttonName = "quit"
         guard let imageButton = UIImage(named: buttonName) else{
             return print("image button not found")
             }
-        let LoboutButton = UIButton.systemButton(
+        let button = UIButton.systemButton(
             with: imageButton,
             target: self,
             action: #selector(Self.didTapButton)
         )
         //BUTTON
-        LoboutButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(LoboutButton)
-        LoboutButton.tintColor = .ypRed
-        LoboutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
-        LoboutButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor).isActive = true
-        LoboutButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
-        LoboutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.tintColor = .ypRed
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        button.centerYAnchor.constraint(equalTo: image.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
         
     }
     @objc
